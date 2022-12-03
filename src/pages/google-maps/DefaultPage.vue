4541d31d03c70c60224a32d6aeef52f1507e6a80
Christher Lenander <christher.lenander@enklare.se>
2022-12-03 17:28:41 +0100

    Init

---
+++ src/pages/google-maps/DefaultPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="">
+    <div class="fit">
+      <GMapMap
+        :center="map.center"
+        :zoom="map.zoom"
+        style="height: calc(100vh - 50px)"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const map = reactive({
+  center: { lat: 59.7593389, lng: 18.7015669 },
+  zoom: 14,
+});
+</script>
